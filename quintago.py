8141f3f5622dbea466ab21df2cb76ab982876366
GabyTheBaby <246833790+BobbyErard-oss@users.noreply.github.com>
2026-03-24 15:48:26 +0100

    PROJET PRESQUE TERMINE, RESTE PLUS QU'A SAUVEGARDER PENDANT LA PARTIE DES QUE L'UTILISATEUR VEUT

--- quintago.py
+++ quintago.py
@@ -1,29 +1,30 @@
-from saisie import *
 from initialisation import *
 from tour_de_jeu import *
 from parametres import *
 from affichage import *
 from tests_alignements import *
+from sauvegarde import *
 
 
-def jouer(): 
-   joueur1 = input("Veuillez saisir le nom du premier joueur") 
-   joueur2 = input("Veuillez saisir le nom du second joueur") 
-   print(joueur1, joueur2, "bienvenue au quintago de Joseph et Gabriel") 
+def jouer(grille): 
+   joueur1 = input("Veuillez saisir le nom du premier joueur : ") 
+   joueur2 = input("Veuillez saisir le nom du second joueur : ") 
+   print(joueur1, joueur2, "bienvenue au Quintago !")
+   afficher_grille(grille)
    
    nombre_tours = 0 
-   while test_victoire() == 0 and nombre_tours < 36:  # tant qu'il n'y a pas de gagnant ou que la limite de tours n'est pas atteinte c'est à dire que toutes les cases de la grille ont été joués 
-      print(joueur1,"A votre tour de jouer") 
-      jouer_tour(1) 
+   while test_victoire(grille) == 0 and nombre_tours < 36:  # tant qu'il n'y a pas de gagnant ou que la limite de tours n'est pas atteinte c'est à dire que toutes les cases de la grille ont été joués 
+      print("A votre tour,", joueur1) 
+      jouer_tour(grille, 1)
       afficher_grille(grille) 
       nombre_tours += 1    
 
-      print(joueur2,"A votre tour de jouer") 
-      jouer_tour(2) 
+      print("A votre tour,", joueur2) 
+      jouer_tour(grille, 2)
       afficher_grille(grille) 
       nombre_tours += 1 
    
-   gagnant = test_victoire() 
+   gagnant = test_victoire(grille) 
    if gagnant == 0:               # on dissocie les cas pour après exécuter un print en fonction du cas
       print("Egalité. Aucun joueur est victorieux.") 
    
@@ -34,4 +35,8 @@ def jouer():
       if gagnant == 1:
          print("Bravo,", joueur1, "vous avez gagné(e)")      
       else:          
-         print("Bravo,", joueur2, "vous avez gagné(e)") 
+         print("Bravo,", joueur2, "vous avez gagné(e)")
+
+if __name__ == "__main__":
+   grille = initialisation_grille()
+   jouer(grille)
--- saisie.py
+++ saisie.py
@@ -2,14 +2,14 @@ from tour_de_jeu import *
 from initialisation import *
 
 # instruction 3 (i) 
-def jouer_case(nb_joueur): 
+def jouer_case(grille, nb_joueur): 
    test_coordonnées = 0 
    while test_coordonnées == 0: 
       test = 0      # remplace le role d'un break 
       
       while test == 0:  # on force la saisie 
          try:       # permet de tester le type de la saisie : dans ce cas précis si ce n'est pas un entier on passe directement aux lignes 31 et 32 qui indiquent que la saisie est invalide 
-            jouer_ligne = int(input("Veuillez indiquer la ligne de la case à jouer: par exemple 1 pour la première ligne en partant du haut")) 
+            jouer_ligne = int(input("Rentrez la ligne : ")) 
             if 1 <= jouer_ligne <= 6: 
                test = 1  # sortie si saisie valide  
          except ValueError:        # cas de saisie invalide (pas un entier) 
@@ -18,7 +18,7 @@ def jouer_case(nb_joueur):
       test = 0 
       while test == 0: 
          try: 
-            jouer_colonne = int(input("Veuillez indiquer la colonne de la case à jouer: par exemple 1 pour la première colonne en partant de la gauche")) 
+            jouer_colonne = int(input("Rentez la colonne : ")) 
             if 1 <= jouer_colonne <= 6: 
                test = 1  # sortie si saisie valide  
          except ValueError: 
@@ -27,14 +27,26 @@ def jouer_case(nb_joueur):
       # on créer une variable gagnant qui permettra de vérifier facilement si la case a déjà été jouée  
       if jouer_colonne <= 3:    # on sépare les 4 sous-listes pour faciliter la lisibilité et la compréhension du code 
          if jouer_ligne <= 3:    
-            gagnant = L1[jouer_ligne-1][jouer_colonne-1]   # on soustrait 1 car dans la saisie de l'utilisateur les lignes et les colonnes commençaient à 1    
+            case = grille[0][0][jouer_ligne-1][jouer_colonne-1]   # on soustrait 1 car dans la saisie de l'utilisateur les lignes et les colonnes commençaient à 1    
          else: 
-            gagnant = L3[jouer_ligne-1-3][jouer_colonne-1]  # on enlève 1 pour la raison précédente et encore 3 car la ligne 4 du joueur(saisie) correspond à l'indice 0 de L3 et ainsi de suite (décalge de 3) 
+            case = grille[1][0][jouer_ligne-1-3][jouer_colonne-1]  # on enlève 1 pour la raison précédente et encore 3 car la ligne 4 du joueur(saisie) correspond à l'indice 0 de L3 et ainsi de suite (décalge de 3) 
       else: 
          if jouer_ligne <= 3: 
-            gagnant = L2[jouer_ligne-1][jouer_colonne-1-3]  # on enlève à jouer_colonne 3 car la colonne 4 du joueur(saisie) correspond à l'indice 0 de L2 et ainsi de suite (décalge de 3) 
+            case = grille[0][1][jouer_ligne-1][jouer_colonne-1-3]  # on enlève à jouer_colonne 3 car la colonne 4 du joueur(saisie) correspond à l'indice 0 de L2 et ainsi de suite (décalge de 3) 
          else: 
-            gagnant = L4[jouer_ligne-1-3][jouer_colonne-1-3]
+            case = grille[1][1][jouer_ligne-1-3][jouer_colonne-1-3]
 
-      if gagnant != "1" and gagnant != "2": # on vérifie que la case saisie n'a pas déjà été jouée
+      if case != 1 and case != 2: # on vérifie que la case saisie n'a pas déjà été jouée
          test_coordonnées = 1      # cela nous permettra de sortir de la boucle while 
+
+   # modification réelle de la grille (corrigé)
+   if jouer_colonne <= 3:
+      if jouer_ligne <= 3:
+         grille[0][0][jouer_ligne-1][jouer_colonne-1] = nb_joueur
+      else:
+         grille[1][0][jouer_ligne-1-3][jouer_colonne-1] = nb_joueur
+   else:
+      if jouer_ligne <= 3:
+         grille[0][1][jouer_ligne-1][jouer_colonne-1-3] = nb_joueur
+      else:
+         grille[1][1][jouer_ligne-1-3][jouer_colonne-1-3] = nb_joueur
